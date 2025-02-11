34e3d84f8a2515e98d372f88dd7ee78b8601baa5
valy04e <geovali99@yahoo.comm>
2025-02-11 13:49:42 +0200

    s

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -11,3 +11,53 @@ window.addEventListener("scroll", function () {
 
 // - - - - - SLIDER PROJECTS - - - - -
 
+let currentIndex = 0;
+const projectsWrapper = document.querySelector('.wrp-projects');
+const dots = document.querySelectorAll('.dot');
+const leftBtn = document.querySelector('.nav-btn.left');
+const rightBtn = document.querySelector('.nav-btn.right');
+const totalItems = document.querySelectorAll('.wrp-projects .project').length; 
+
+function changeSlide(index) {
+    if (index < 0) {
+        currentIndex = totalItems - 1;
+    } else if (index >= totalItems) {
+        currentIndex = 0; 
+    } else {
+        currentIndex = index;
+    }
+
+    const newPosition = 385 * currentIndex;
+
+    projectsWrapper.scrollTo({
+        left: newPosition,
+        behavior: 'smooth',
+    });
+
+    dots.forEach((dot, i) => dot.checked = i === currentIndex);
+}
+
+leftBtn.addEventListener('click', () => changeSlide(currentIndex - 1));
+
+rightBtn.addEventListener('click', () => changeSlide(currentIndex + 1));
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('change', () => changeSlide(index));
+});
+
+projectsWrapper.addEventListener('wheel', (e) => {
+    if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
+        
+        return;
+    } else {
+        
+        e.preventDefault();
+
+        
+        if (e.deltaX > 0) {
+            changeSlide(currentIndex + 1); 
+        } else {
+            changeSlide(currentIndex - 1);
+        }
+    }
+});
