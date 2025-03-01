5c04ee1b9cb25a2ec4be0c257ccd2c5b0aa0725b
valy04e <geovali99@yahoo.comm>
2025-03-01 02:11:26 +0200

    Co-authored-by: Alex <rotarualexandru22@users.noreply.github.com>

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -11,53 +11,25 @@ window.addEventListener("scroll", function () {
 
 // - - - - - SLIDER PROJECTS - - - - -
 
-let currentIndex = 0;
-const projectsWrapper = document.querySelector('.wrp-projects');
-const dots = document.querySelectorAll('.dot');
-const leftBtn = document.querySelector('.nav-btn.left');
-const rightBtn = document.querySelector('.nav-btn.right');
-const totalItems = document.querySelectorAll('.wrp-projects .project').length; 
+// document.addEventListener("DOMContentLoaded", function () {
+//     const container = document.querySelector(".wrp-projects");
+//     const leftBtn = document.querySelector(".nav-btn.left");
+//     const rightBtn = document.querySelector(".nav-btn.right");
+//     const firstArticle = container.querySelector(".project");
 
-function changeSlide(index) {
-    if (index < 0) {
-        currentIndex = totalItems - 1;
-    } else if (index >= totalItems) {
-        currentIndex = 0; 
-    } else {
-        currentIndex = index;
-    }
+//     const calculateScrollAmount = () => {
+//         const articleWidth = firstArticle.offsetWidth;
+//         const marginRight = parseInt(window.getComputedStyle(container).marginRight.replace("px", ""));
+//         return articleWidth + marginRight;
+//     };
 
-    const newPosition = 385 * currentIndex;
+//     rightBtn.addEventListener("click", function () {
+//         const scrollAmount = calculateScrollAmount();
+//         container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+//     });
 
-    projectsWrapper.scrollTo({
-        left: newPosition,
-        behavior: 'smooth',
-    });
-
-    dots.forEach((dot, i) => dot.checked = i === currentIndex);
-}
-
-leftBtn.addEventListener('click', () => changeSlide(currentIndex - 1));
-
-rightBtn.addEventListener('click', () => changeSlide(currentIndex + 1));
-
-dots.forEach((dot, index) => {
-    dot.addEventListener('change', () => changeSlide(index));
-});
-
-projectsWrapper.addEventListener('wheel', (e) => {
-    if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-        
-        return;
-    } else {
-        
-        e.preventDefault();
-
-        
-        if (e.deltaX > 0) {
-            changeSlide(currentIndex + 1); 
-        } else {
-            changeSlide(currentIndex - 1);
-        }
-    }
-});
+//     leftBtn.addEventListener("click", function () {
+//         const scrollAmount = calculateScrollAmount();
+//         container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+//     });
+// });
