dcbb593c939bc5307693862068f61fb637175275
valy04e <geovali99@yahoo.comm>
2025-03-24 15:16:59 +0200

    w

--- js/index.js
+++ js/index.js
@@ -1,18 +1,10 @@
-let myH1 = document.getElementById("myH1");
-let myForm = document.getElementById("myForm");
-let myBTN = document.getElementById("myBTN");
-let username = document.getElementById("username");
-let password = document.getElementById("password");
-
-myBTN.onclick = function() {
-    let usernameValue = username.value;
-    let passwordValue = password.value;
-
-    if(usernameValue === "admin" && passwordValue === "hartie") {
-        myH1.textContent = `Welcome, ${usernameValue}`;
-        myForm.style.display = "none";
-    }
-    else {
-        myH1.textContent = `ERROR, try again!`;
-    }
+function afiseazaMesaj() {
+    console.log("Salut! Am fost apelat mai târziu.");
 }
+
+function executaMaiTârziu(callback) {
+    console.log("Fac ceva acum...");
+    callback(); // Apelează funcția trimisă ca argument
+}
+
+executaMaiTârziu(afiseazaMesaj);
